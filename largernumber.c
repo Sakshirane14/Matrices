f1214c29eaa3dae867031838b9bc3fa11b764c57
Sakshirane14 <144129093+Sakshirane14@users.noreply.github.com>
2023-10-16 21:26:35 +0530

    Update largernumber.c

--- largernumber.c
+++ largernumber.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+  int arr[]={5,80,40,60,90,100,150,50);
+int length=sizeof(arr)/sizeof(arr[0]));
+int maz=arr[0];
+for(int i=0;i<length;i++){
+if(arr[i]>max){
+max=arr[i];
+}
+}
+return 0;
+}
